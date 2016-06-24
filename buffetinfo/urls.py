932dd1dbd43c02c8db73c6a1f3cb665e8985ee83
leeyh20 <leeyh20@hotmail.com>
2016-06-24 12:04:08 +0800

    Added view and template for detailed blog post as well as CSS for the site

--- buffetinfo/urls.py
+++ buffetinfo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.buffet_list, name='buffet_list'),
+    url(r'^buffet/(?P<pk>\d+)/$', views.buffet_detail, name = 'buffet_detail'),
 ]
--- buffetinfo/views.py
+++ buffetinfo/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Buffet
 
 def buffet_list(request):
 	buffets = Buffet.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'buffetinfo/buffet_list.html', {'buffets' : buffets})
+
+def buffet_detail(request, pk):
+    buffet = get_object_or_404(Buffet, pk=pk)
+    return render(request, 'buffetinfo/buffet_detail.html', {'buffet': buffet})
