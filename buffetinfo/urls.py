0df4b0f3a91b55563617b6053cddfaa000ce00c9
leeyh20 <leeyh20@hotmail.com>
2016-06-26 03:21:02 +0800

    done the first extension to have a publish button

--- buffetinfo/urls.py
+++ buffetinfo/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     url(r'^$', views.buffet_list, name='buffet_list'),
     url(r'^buffet/(?P<pk>\d+)/$', views.buffet_detail, name = 'buffet_detail'),
     url(r'^buffet/new/$', views.buffet_new, name='buffet_new'),
+    url(r'^buffet/(?P<pk>\d+)/edit/$', views.buffet_edit, name='buffet_edit'),
+    url(r'^drafts/$', views.buffet_draft_list, name='buffet_draft_list'),
+    url(r'^buffet/(?P<pk>\d+)/publish/$', views.buffet_publish, name='buffet_publish'),
+    url(r'^buffet/(?P<pk>\d+)/remove/$', views.buffet_remove, name='buffet_remove'),
 ]
--- buffetinfo/views.py
+++ buffetinfo/views.py
@@ -20,9 +20,38 @@ def buffet_new(request):
             buffet = form.save(commit=False)
             buffet.author = request.user
             buffet.rating = 0.0
-            buffet.published_date = timezone.now()
+            #buffet.published_date = timezone.now()
             buffet.save()
             return redirect('buffet_detail', pk=buffet.pk)
     else:
         form = BuffetForm()
     return render(request, 'buffetinfo/buffet_edit.html', {'form': form})
+
+def buffet_edit(request, pk):
+    buffet = get_object_or_404(Buffet, pk=pk)
+    if request.method == "POST":
+        form = BuffetForm(request.POST, instance=buffet)
+        if form.is_valid():
+            buffet = form.save(commit=False)
+            buffet.author = request.user
+            buffet.rating = 0.0
+            #buffet.published_date = timezone.now()
+            buffet.save()
+            return redirect('buffet_detail', pk=buffet.pk)
+    else:
+        form = BuffetForm(instance=buffet)
+    return render(request, 'buffetinfo/buffet_edit.html', {'form': form})
+
+def buffet_draft_list(request):
+    buffets = Buffet.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'buffetinfo/buffet_draft_list.html', {'buffets': buffets})
+
+def buffet_publish(request, pk):
+    buffet = get_object_or_404(Buffet, pk=pk)
+    buffet.publish()
+    return redirect('buffetinfo.views.buffet_detail', pk=pk)
+
+def buffet_remove(request, pk):
+    buffet = get_object_or_404(Buffet, pk=pk)
+    buffet.delete()
+    return redirect('buffetinfo.views.buffet_list')
